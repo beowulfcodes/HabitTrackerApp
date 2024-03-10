3f0d1bc5a1fac46024c3925439e652d249e5744f
Aaron A. Santillan <139300769+beowulfcodes@users.noreply.github.com>
2024-03-10 21:44:07 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Globalization;
+
+namespace HabitTrackerApp
+{
+    class Program
+    {
+        static string connectionString = @"Data Source=habittracker.db";
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS drinking_water (
+                                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            Date TEXT,
+                                            Quantity INTEGER
+                                        )";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            Menu();
+        }
+
+        static void Menu()
+        {
+            bool closeApp = false;
+
+            while (closeApp == false)
+            {
+                Console.WriteLine("Welcome to Habit Tracker!");
+                Console.WriteLine("What would you like to do?\n");
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("Type 0 to Close the application.");
+                Console.WriteLine("Type 1 to View all records.");
+                Console.WriteLine("Type 2 to Insert a new record.");
+                Console.WriteLine("Type 3 to Delete a record.");
+                Console.WriteLine("Type 4 to Update a record.");
+                Console.WriteLine("------------------------------------------");
+
+
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "0":
+                        Console.WriteLine("Goodbye!");
+                        closeApp = true;
+                        Environment.Exit(0);
+                        break;
+                    case "1":
+                        GetAllRecords();
+                        break;
+                    case "2":
+                        Insert();
+                        break;
+                    case "3":
+                        Delete();
+                        break;
+                    case "4":
+                        Update();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input! Please enter a number from 0 to 4!\n");
+                        break;
+                }
+            }
+        }
+
+        private static void GetAllRecords()
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                string createTableQuery = $"SELECT * FROM drinking_water";
+                tableCmd.CommandText = createTableQuery;
+
+                List<DrinkingWater> tableData = new();
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        tableData.Add(
+                            new DrinkingWater
+                            {
+                                Id = reader.GetInt32(0),
+                                Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
+                                Quantity = reader.GetInt32(2)
+                            }); ;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found");
+                }
+
+                connection.Close();
+
+                Console.Clear();
+                Console.WriteLine("View All Records: \n");
+                Console.WriteLine("------------------------------------------");
+                foreach (var dw in tableData)
+                {
+                    Console.WriteLine($"{dw.Id} - {dw.Date.ToString("dd-MMM-yyyy")} - Quantity: {dw.Quantity}");
+                }
+                Console.WriteLine("------------------------------------------\n");
+            }
+        }
+
+        private static void Insert()
+        {
+            Console.Clear();
+            string date = GetDateInput();
+            int quantity = GetNumberInput("\n\nPlease insert a number of glasses or other measure of your choice (no decimals allowed)");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = $"INSERT INTO drinking_water (date, quantity) VALUES('{date}', '{quantity}')";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            Console.WriteLine("\nA new record has been inserted!\n\n");
+        }
+
+        private static void Delete()
+        {
+            Console.Clear();
+            GetAllRecords();
+            var recordId = GetNumberInput("Please type the Id of the record you want to delete or type 0 to go back to main menu.");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = $"DELETE from drinking_water WHERE Id = '{recordId}'";
+
+                int rowCount = tableCmd.ExecuteNonQuery();
+
+                if (rowCount == 0)
+                {
+                    Console.WriteLine($"\n\nRecord with Id {recordId} doesn't exist. \n\n");
+                    Delete();
+                }
+            }
+
+            Console.WriteLine($"\n\nRecord with Id {recordId} was deleted. \n\n");
+
+            Menu();
+        }
+
+        internal static void Update()
+        {
+            GetAllRecords();
+            var recordId = GetNumberInput("Please type Id of the record would like to update. Type 0 to return to main menu.");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var checkCmd = connection.CreateCommand();
+                checkCmd.CommandText = $"SELECT EXISTS(SELECT 1 FROM drinking_water WHERE Id = {recordId})";
+                int checkQuery = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                if (checkQuery == 0)
+                {
+                    Console.WriteLine($"\n\nRecord with Id {recordId} doesn't exist.\n\n");
+                    connection.Close();
+                    Update();
+                }
+
+                string date = GetDateInput();
+                int quantity = GetNumberInput("\n\nPlease insert number of glasses or other measure of your choice (no decimals allowed)");
+
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = $"UPDATE drinking_water SET date = '{date}', quantity = {quantity} WHERE Id = {recordId}";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+
+                Console.WriteLine($"\nThe record with Id {recordId} has been updated.\n\n");
+            }
+        }
+
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to main menu.");
+
+            string dateInput = Console.ReadLine();
+
+            if (dateInput == "0") Menu();
+
+            return dateInput;
+        }
+
+        internal static int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+            string numberInput = Console.ReadLine();
+
+            if (numberInput == "0") Menu();
+
+            while (!Int32.TryParse(numberInput, out _) || Convert.ToInt32(numberInput) < 0)
+            {
+                Console.WriteLine("\n\nInvalid number. Try again.\n\n");
+                numberInput = Console.ReadLine();
+            }
+
+            int finalInput = Convert.ToInt32(numberInput);
+            return finalInput;
+        }
+    }
+
+    public class DrinkingWater
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+    }
+}
